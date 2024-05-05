8ce11069376358e67a9700065810c1ebdd7b50dc
Oxana Chizitcaia <creativesouloxana@gmail.com>
2024-05-05 16:48:47 +0200

    Added function max3

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
+// const a = Number(prompt('Zadejte první číslo: '));
+// const b = Number(prompt('Zadejte druhé číslo: '));
+
+// const max2 = (a, b) => {
+//   if (a > b) {
+//     return a;
+//   } else {
+//     return b;
+//   }
+// };
+
+// document.body.innerHTML += `Větší číslo z vámi zadaných dvou čísel je číslo: ${max2(
+//   a,
+//   b,
+// )} `;
+
 const a = Number(prompt('Zadejte první číslo: '));
 const b = Number(prompt('Zadejte druhé číslo: '));
+const c = Number(prompt('Zadejte třetí číslo: '));
 
-const max2 = (a, b) => {
+const max3 = (a, b, c) => {
   if (a > b) {
     return a;
-  } else {
+  } else if (b > c) {
     return b;
+  } else {
+    return c;
   }
 };
 
-document.body.innerHTML += `Větší číslo z vámi zadaných dvou čísel je číslo: ${max2(
+document.body.innerHTML += `Větší číslo z vámi zadaných tří čísel je číslo: ${max3(
   a,
   b,
+  c,
 )} `;
